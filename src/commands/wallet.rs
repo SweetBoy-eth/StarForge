ee3e1d69922d9c02e13af0ce9d4ca82fc625429c
nonso7 <felixmedia78@gmail.com>
2026-06-22 15:51:56 +0100

    style: apply cargo fmt formatting standards

--- src/commands/wallet.rs
+++ src/commands/wallet.rs
@@ -364,7 +364,17 @@ pub fn handle(cmd: WalletCommands) -> Result<()> {
             iterations,
             parallelism,
             backup,
-        } => rotate_wallet(name, fund, network, encrypt, strict, mem, iterations, parallelism, backup),
+        } => rotate_wallet(
+            name,
+            fund,
+            network,
+            encrypt,
+            strict,
+            mem,
+            iterations,
+            parallelism,
+            backup,
+        ),
         WalletCommands::Export {
             name,
             all,
